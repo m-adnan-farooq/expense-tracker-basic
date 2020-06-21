204fa0d0da1226fb3d5f22d74bfbfe061e36a819
m-adnan-farooq <m.adnan.far@gmail.com>
2020-06-21 09:16:15 +0500

    Basic CSS applied

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return <div>
-        <h1>Expense Tracker</h1>
+        <h1>Expense Tracker Application</h1>
     </div>
 }
 
--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -5,18 +5,26 @@ import {allTransactions} from '../GlobalContext';
 export const NewTransaction = () => {
     const transactions = useContext(GlobalContext);
     function income(){
+        if (document.getElementById('amount').value>0){
         var particulars =  document.getElementById('particulars').value;
         var amount = document.getElementById('amount').value;
         allTransactions.push({'income': [particulars, amount]})
-        transactions[1](JSON.parse(JSON.stringify(allTransactions)))
+        transactions[1](JSON.parse(JSON.stringify(allTransactions)))}
+        else{
+            alert('Please enter a value greater than zero')
+        }
         // console.log(transactions[0])
     }
 
     function expense(){
+        if (document.getElementById('amount').value>0){
         var particulars =  document.getElementById('particulars').value;
         var amount = document.getElementById('amount').value;
         allTransactions.push({'expense': [particulars, amount]})
-        transactions[1](JSON.parse(JSON.stringify(allTransactions)))
+        transactions[1](JSON.parse(JSON.stringify(allTransactions)))}
+        else{
+            alert('Please enter a value greater than zero')
+        }
         // console.log(transactions[0])
     }
     return (
@@ -24,9 +32,11 @@ export const NewTransaction = () => {
             <h4>Add New Transaction</h4>
             <form className = 'new-transaction'>
                 <input id = 'particulars' type = 'text' placeholder = 'Enter transaction particulars' required/>
+                <br />
                 <input id = 'amount' type = 'number' placeholder = 'Enter amount' required/>
-                <input type = 'button' value = 'Income' onClick={income} />
-                <input type = 'button' value = 'Expense' onClick={expense} />
+                <br />
+                <input className='btn' type = 'button' value = 'Income' onClick={income} />
+                <input className='btn' type = 'button' value = 'Expense' onClick={expense} />
                 <p>(Click on appropriate button to record the transaction)</p>
             </form>
         </div>
